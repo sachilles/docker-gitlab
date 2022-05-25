99b5174c346e423d5e95fb8199f242bb81bf36d6
Kazunori Kimura <norytama.k2@gmail.com>
2022-05-26 08:28:55 +0900

    WIP: Sync upstream config based on gitlab-foss v15.0.0
    required versions of components are:
    - gitaly: v15.0.0
    - gitlab-shell: v14.3.0
    - gitlab-pages: v1.58.0
    
    TODO:
    - [ ]add update process to assets/runtime/function
    - [ ] parameterize
    - [ ] re-check
    - [ ] check compatibility for renamed parameter

--- assets/runtime/config/gitlabhq/puma.rb
+++ assets/runtime/config/gitlabhq/puma.rb
@@ -1,5 +1,3 @@
-ENV['RAILS_RELATIVE_URL_ROOT'] = "{{GITLAB_RELATIVE_URL_ROOT}}"
-
 # frozen_string_literal: true
 
 # Load "path" as a rackup file.
@@ -34,7 +32,6 @@ queue_requests false
 # Bind the server to "url". "tcp://", "unix://" and "ssl://" are the only
 # accepted protocols.
 bind 'unix:///home/git/gitlab/tmp/sockets/gitlab.socket'
-bind 'tcp://127.0.0.1:8080'
 
 workers {{PUMA_WORKERS}}
 
@@ -48,7 +45,7 @@ end
 
 before_fork do
   # Signal to the puma killer
-  Gitlab::Cluster::PumaWorkerKillerInitializer.start(@config.options, puma_per_worker_max_memory_mb: {{PUMA_PER_WORKER_MAX_MEMORY_MB}}, puma_master_max_memory_mb: {{PUMA_MASTER_MAX_MEMORY_MB}}) unless ENV['DISABLE_PUMA_WORKER_KILLER']
+  Gitlab::Cluster::PumaWorkerKillerInitializer.start @config.options unless ENV['DISABLE_PUMA_WORKER_KILLER']
 
   # Signal application hooks that we're about to fork
   Gitlab::Cluster::LifecycleEvents.do_before_fork
@@ -72,10 +69,25 @@ tag 'gitlab-puma-worker'
 #
 worker_timeout {{PUMA_TIMEOUT}}
 
+# https://github.com/puma/puma/blob/master/5.0-Upgrade.md#lower-latency-better-throughput
+wait_for_less_busy_worker ENV.fetch('PUMA_WAIT_FOR_LESS_BUSY_WORKER', 0.001).to_f
+
+# https://github.com/puma/puma/blob/master/5.0-Upgrade.md#nakayoshi_fork
+nakayoshi_fork unless ENV['DISABLE_PUMA_NAKAYOSHI_FORK'] == 'true'
+
 # Use json formatter
 require_relative "{{GITLAB_INSTALL_DIR}}/lib/gitlab/puma_logging/json_formatter"
 
 json_formatter = Gitlab::PumaLogging::JSONFormatter.new
 log_formatter do |str|
   json_formatter.call(str)
+end
+
+lowlevel_error_handler do |ex, env|
+  if Raven.configuration.capture_allowed?
+    Raven.capture_exception(ex, tags: { 'handler': 'puma_low_level' }, extra: { puma_env: env })
+  end
+
+  # note the below is just a Rack response
+  [500, {}, ["An error has occurred and reported in the system's low-level error handler."]]
 end
